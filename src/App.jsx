8b831584c39812c9eb65af933723b26b0d9471fa
Hybola <keeratipong.b@gmail.com>
2024-08-04 21:15:37 +0700

    lecture 289: error message when fail to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,21 @@ import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
+import Error from "./ui/Error";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/:orderId", element: <Order /> },
+      { path: "/order/:orderId", element: <Order />, errorElement: <Error /> },
       { path: "/order/new", element: <CreateOrder /> },
     ],
   },
