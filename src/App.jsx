e33c4f9ea839b48b08c78cfbd7f995e70ba4e7af
Hybola <keeratipong.b@gmail.com>
2024-08-05 16:46:58 +0700

    lecture 290: search order

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import AppLayout from "./ui/AppLayout";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import Error from "./ui/Error";
 const router = createBrowserRouter([
@@ -19,7 +19,12 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/:orderId", element: <Order />, errorElement: <Error /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        errorElement: <Error />,
+        loader: orderLoader,
+      },
       { path: "/order/new", element: <CreateOrder /> },
     ],
   },
