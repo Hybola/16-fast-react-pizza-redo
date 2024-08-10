caa4242ca8405088946a19e711b1b9abf3384448
Hybola <keeratipong.b@gmail.com>
2024-08-10 13:32:11 +0700

    lecture 298

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Boonnapongkasem</div>;
+  return <div className="hidden text-sm font-semibold">Boonnapongkasem</div>;
 }
 
 export default Username;
